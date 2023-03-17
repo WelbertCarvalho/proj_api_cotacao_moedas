3be6833f5f1ca0db318e9e71edda3c7efc29401d
Welbert <welbertsilveiracarvalho@gmail.com>
2023-03-17 15:36:25 -0300

    Renomeando função get_json_data

--- conexao_api.py
+++ conexao_api.py
@@ -5,7 +5,7 @@ class Extrator_dados_api:
     def __init__(self):
         print("Instância do objeto de extração\n")
 
-    def get_json_data(self, url):
+    def captura_dados_json(self, url):
         r = requests.get(url)
         json_data = json.loads(r.text)
         return json_data
@@ -14,6 +14,6 @@ class Extrator_dados_api:
 
 cotacao_usd_brl = Extrator_dados_api()
 
-retorno = cotacao_usd_brl.get_json_data("https://economia.awesomeapi.com.br/last/USD-BRL")
+retorno = cotacao_usd_brl.captura_dados_json("https://economia.awesomeapi.com.br/last/USD-BRL")
 
 print(retorno)
