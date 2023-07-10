130dccc16b877f08e96c76a598d4d9c9355d62b2
welbert <welbertsilveiracarvalho@gmail.com>
2023-07-10 19:31:19 -0300

    Criando um dict comprehension de retorno da função extrai_cotacao_diaria

--- api_cotacoes.py
+++ api_cotacoes.py
@@ -1,25 +1,29 @@
 import sys
 sys.path.append("/home/welbert/projetos/airflow")
 
-from pipelines.proj_api_cotacao_moedas.extracao_api import Extrator_dados_api
+from pipelines.proj_api_cotacao_moedas.extracao import Extrator_dados
 from pipelines.proj_api_cotacao_moedas.conexao_db import Conexao
 
-def dados_cotacao_diaria(origem: str, destino: str) -> None:
-    cotacao_usd_brl = Extrator_dados_api()
-    retorno = cotacao_usd_brl.captura_dados_json(f'https://economia.awesomeapi.com.br/last/{origem}-{destino}')
-    print(retorno)
 
-    df = cotacao_usd_brl.cria_df_usando_dict(retorno)
-    print(df)
+def extrai_cotacao_diaria(origem: str, destino: str) -> None:
+    cotacao_usd_brl = Extrator_dados()
+    retorno = cotacao_usd_brl.captura_dados_json(f'https://economia.awesomeapi.com.br/last/{origem}-{destino}')
+    campos = ['code', 'codein', 'name', 'high', 'low', 'varBid', 'pctChange', 'bid', 'ask', 'create_date']
+    retorno_filtrado = {chave: valor for chave, valor in retorno[f"{origem}{destino}"].items() if chave in campos}
+    print(retorno_filtrado)
 
     datalake = Conexao()
     datalake_con = datalake.conexao_sqlalchemy(
         'datalake',
         'mysql+pymysql'
     )
-
     print(datalake_con)
 
 
 
+    return None
+
+
+if __name__ == '__main__':
+    captura = extrai_cotacao_diaria(origem = 'USD', destino = 'BRL')
 
---
+++ extracao.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import requests
+import json
+import pandas as pd
+
+import sys
+sys.path.append("/home/welbert/projetos/airflow")
+from pipelines.proj_api_cotacao_moedas.conexao_db import Conexao
+
+
+class Extrator_dados:
+    def __init__(self):
+        print('Instância do objeto de extração')
+
+    def captura_dados_json(self, url: str) -> dict:
+        r = requests.get(url)
+        dados_json = json.loads(r.text)
+        return dados_json
+    
+    def cria_df_usando_dict(self, dict_json: dict) -> pd.DataFrame:
+        df = pd.DataFrame(dict_json)
+        return df
+    
+    def lendo_conteudo_sql(self, nome_arquivo_sql: str) -> str:
+        dir_atual = Path().absolute()
+        dir_sql = f"{dir_atual}/sql"
+        arquivo_sql = open(f'{dir_sql}/{nome_arquivo_sql}.sql', 'r')
+        conteudo_sql = arquivo_sql.read()
+        arquivo_sql.close()
+        return conteudo_sql
+    
+    def cria_df_lendo_sql_sem_param(self, nome_arquivo_sql: str, conexao: Conexao.conexao_sqlalchemy) -> pd.DataFrame:
+        conteudo_sql = self._lendo_conteudo_sql(nome_arquivo_sql)
+        df = pd.read_sql_query(conteudo_sql, conexao)
+        conexao.close()
+        return df
+
+
+if __name__ == '__main__':
+    datalake = Conexao()
+    datalake_con = datalake.conexao_sqlalchemy(
+    'datalake',
+    'mysql+pymysql'
+    )
+
+    extrator = Extrator_dados()
+    dados_cot_diaria = extrator.captura_dados_json('https://economia.awesomeapi.com.br/last/USD-BRL')
+    dict_cot_diaria = dados_cot_diaria.values()
+
+    print(dict_cot_diaria)
+
+    df = extrator.cria_df_usando_dict(dict_cot_diaria)
+
+    print(df.head())
